b3a34933f3aeb508bc50f2422aa300b0b23e2c46
zblednu <maksym.surovtsev@student.tuke.sk>
2025-03-11 07:17:58 +0100

    multi-container, multi-port

--- app.ts
+++ app.ts
@@ -4,36 +4,49 @@ import Docker from 'dockerode';
 const app = express();
 const docker = new Docker();
 
-const containerOptions: Docker.ContainerCreateOptions = {
-	Image: 'quay.io/jupyter/base-notebook',
-	ExposedPorts: {
-		'8888/tcp': {}
-	},
-	HostConfig: {
-		PortBindings: {
-			'8888/tcp': [{ HostPort: '8888' }]
+let nextFreePort = 8888;
+const containers: Docker.Container[] = [];
+
+process.on('SIGINT', async () => {
+	const promiseQueue = [];
+	for (const container of containers) {
+		promiseQueue.push(container.stop());
+	}
+	Promise.all(promiseQueue)
+		.then(() => {
+			console.log('boom');
+			process.exit(0)
+		})
+});
+
+function createOptions() {
+	return {
+		Image: 'quay.io/jupyter/base-notebook',
+		ExposedPorts: {
+			'8888/tcp': {}
 		},
-		AutoRemove: true
-	},
-	Cmd: ['start-notebook.py', '--NotebookApp.token=\'mytoken\'']
-};
+		HostConfig: {
+			PortBindings: {
+				'8888/tcp': [{ HostPort: `${nextFreePort++}` }]
+			},
+			AutoRemove: true
+		},
+		Cmd: ['start-notebook.py', '--NotebookApp.token=\'token\'']
+	};
+}
 
 async function createJupyterSession() {
-	const container = await docker.createContainer(containerOptions)
+	const options = createOptions()
+	const container = await docker.createContainer(options)
+	containers.push(container);
 	await container.start();
-	console.log('live!');
-
-	process.on('SIGINT', async () => {
-		await container.stop();
-		console.log('boom');
-		process.exit(0)
-	});
 }
 
 
 app.get('/', async (_, res) => {
+	const redirectTo = `http://localhost:${nextFreePort}/?token=token`;
 	await createJupyterSession();
-	setTimeout(() => res.redirect('http://localhost:8888/?token=mytoken'), 2000);
+	setTimeout(() => res.redirect(redirectTo), 3000);
 })
 
 app.listen(3000, () => console.log('listenng'));
